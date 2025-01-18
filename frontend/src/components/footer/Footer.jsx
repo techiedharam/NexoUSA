f74374c8d59cf551e734275190795681da65089e
Dharmender <dharmender7077@gmail.com>
2025-01-18 14:36:04 +0530

    Contact us page created with responsiveness

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -12,19 +12,19 @@ const Footer = () => {
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
-        filter: "brightness(0.6)"
+        filter: "brightness(0.4)"
       }}
     >
       {/* Main container of footer */}
       <div className=" grid lg:grid-cols-3 grid-cols-1 gap-5 text-white">
         <div>
-          <h1 className=" text-2xl text-[#ff9c00] font-semibold ">ABOUS US</h1>
+          <h1 className=" text-xl text-[#fff] underline font-semibold ">ABOUS US</h1>
           <p className="mt-4 text-white/60">Nexo is an EN 15048 & CE Certified company leading manufacturer of high
             tensile hot dip Galvanized fastener & cold forged Fasteners Manufacturers in India.
           </p>
         </div>
         <div>
-          <h1 className="text-2xl text-[#ff9c00] font-semibold">GET IN TOUCH</h1>
+          <h1 className="text-xl text-[#fff] underline font-semibold">GET IN TOUCH</h1>
           <ul className=" mt-4 text-white/60">
             <li className="flex items-start  gap-x-2 leading-relaxed">
               <MapPinMinusInside />
@@ -41,7 +41,7 @@ const Footer = () => {
           </ul>
         </div>
         <div>
-          <h1 className=" text-2xl text-[#ff9c00] font-semibold">LOCATE US</h1>
+          <h1 className=" text-xl text-[#fff] underline font-semibold">LOCATE US</h1>
           <div className="mt-4 w-full">
             <iframe className=" w-full"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6998.741357153004!2d77.10309279114583!3d28.708466262521114!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d19228e46bd1f%3A0xe1f07fa40f4b2afb!2sGayatri%20Computer%20Charitable%20Trust!5e0!3m2!1sen!2sin!4v1734261834711!5m2!1sen!2sin"
