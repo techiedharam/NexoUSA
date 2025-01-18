ba5c91391b9983c25a63642952d67012b30bd7d3
Dharmender <dharmender7077@gmail.com>
2025-01-18 11:36:18 +0530

    Threaded Rods added inside fasters division

--- frontend/src/pages/subMenusPages/FastenersDivision/ThreadedRods.jsx
+++ frontend/src/pages/subMenusPages/FastenersDivision/ThreadedRods.jsx
@@ -1,11 +1,40 @@
-// import React from 'react'
+// import React from 'react';
+import ThreadedRodsCoverImg from "../../../assets/img/ThreadedRodsCoverImg.jpg";
+import ThreadedRodsManufacturersIndia from "../../../assets/img/ThreadedRodsManufacturersIndia.jpg";
 
 const ThreadedRods = () => {
   return (
-    <div>
-      ThreadedRods
-    </div>
-  )
-}
+    <div> {/* Main Container */}
+      <div> {/* Banner image */}
+        <img
+          src={ThreadedRodsCoverImg}
+          alt="Threaded Rods Cover"
+          className="w-full h-[250px] object-cover bg-center bg-fixed"
+        />
+        <h1 className="py-2 px-5 md:px-20 bg-[#ff9c00] text-2xl font-bold text-white">Threaded Rods</h1>
+      </div>
 
-export default ThreadedRods
+      <h1 className="text-center text-xl md:text-2xl font-bold mt-5">
+        Threaded Rods Manufacturers India, Threaded Rod Manufacturers in Ludhiana
+      </h1>
+
+      <div className="flex flex-col md:flex-row  mt-6 md:py-10 py-4 md:px-20 px-4"> {/* Content Container */}
+        <div className="md:w-1/2"> {/* Left content */}
+          <p className="text-base md:text-lg">
+            Nexo Industries is one of the largest Threaded Rods Manufacturers in India. To manufacture high-quality Threaded Rods for our clients, we use the finest quality materials, precision, and cutting-edge technology. Our company is a renowned stockist of many types of high-quality Threaded Rods for huge businesses and other industries. We supply and export a wide assortment of Threaded Rods in India and a few other countries. We have played a significant role in the development and growth of the Indian market.
+          </p>
+        </div>
+
+        <div className="md:w-1/2 mt-4 md:mt-0"> {/* Right content */}
+          <img
+            src={ThreadedRodsManufacturersIndia}
+            alt="Threaded Rods Manufacturer"
+            className="w-[80%] object-cover rounded-lg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ThreadedRods;
