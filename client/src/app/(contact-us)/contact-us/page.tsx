c65f6b477e432e554b09fe9993ec6fdcfd214c79
Dharmender <dharmender7077@gmail.com>
2025-08-21 11:14:43 +0530

    some other pages content added

--- client/src/app/(contact-us)/contact-us/page.tsx
+++ client/src/app/(contact-us)/contact-us/page.tsx
@@ -87,7 +87,7 @@ const ContactUs = () => {
           width="100%"
           height="100%"
           style={{ border: "0" }}
-          allowFullScreen=""
+          // allowFullScreen=""
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
